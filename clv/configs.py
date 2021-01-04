5b858bb1439d0bb944894035d5aee4d800009264
Caglan <cakpinar23@gmail.com>
2021-01-04 03:49:11 +0300

    some irrelevant variables functions are removed from configs.py

--- clv/configs.py
+++ clv/configs.py
@@ -1,40 +1,8 @@
-import datetime
 import yaml
 from os.path import join, abspath
-from dateutil.parser import parse
-
-
-init_directory = abspath(__file__).split("configs.py")[0]  # "/".join(abspath(__file__).split("/")[:-1]) + "/"
-init_directory2 = abspath("")
-
-
-def get_directory(path):
-    directory, web, web_host = abspath(__file__).split("configs.py")[0], 7000, '127.0.0.1'
-    with open(join(path, "instance.yaml")) as file:
-        instances = yaml.full_load(file)
-    active_ins = []
-    for ins in instances['instances']:
-        if ins['active'] is True and init_directory2 == ins['absolute_path']:
-            active_ins.append(ins)
-    if len(active_ins) == 0:
-        directory = init_directory
-        web = 7070
-    if len(active_ins) == 1:
-        active = active_ins[0]
-        directory = active['directory']
-        web = active['web']
-        web_host = active['web_host']
-    if len(active_ins) > 1:
-        now = datetime.datetime.now()
-        actives = list(map(lambda x: ((now - x['start_date']).total_seconds(), x['directory'], x['web'], x['web_host']), active_ins))
-        directory = sorted(actives)[0][1]
-        web = sorted(actives)[0][2]
-        web_host = sorted(actives)[0][3]
-    return directory, web, web_host
 
 
 def read_config(directory):
-
     with open(join(directory, "docs", "configs.yaml")) as file:
         config = yaml.full_load(file)
     return config
