332c36c66730e42f906cae42eccee5baaa957188
caglanakpinar <cakpinar23@gmail.com>
2021-06-19 15:41:26 +0300

    setup.py is updated

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clv_prediction",
-    version="0.1.4",
+    version="0.1.5",
     author="Caglan Akpinar",
     author_email="cakpinar23@gmail.com",
     description="clv prediction applying with deep learning",
